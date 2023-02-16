35a7e93a8bb1b7478dfc791170eff37649396a37
Maria Claudia Rincon <maria-claudiarincon@hotmail.com>
2023-02-16 16:28:53 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaBancarioExemplo/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using SistemaBancarioExemplo.SistemaBancario;
+
+namespace SistemaBancarioExemplo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conta = new Conta();
+            conta.NomeConta = "Conta teste";
+            conta.Agencia = 0001;
+            conta.NumeroConta = 1000;
+            conta.SaldoConta= 1000;
+
+            conta.Titular = new Titular()
+            {
+                CPF = "998877661212",
+                RG = "9912345",
+                Endereco = "Rua 01",
+                Nome = "Jesus"
+            };
+
+           Console.WriteLine($"O saldo em conta é: {conta.VerSaldo()}");
+
+            conta.Depositar(200);
+
+
+            Console.WriteLine($"O saldo em conta é: {conta.VerSaldo()}");
+
+            conta.Sacar(50);
+
+
+            Console.WriteLine($"O saldo em conta é: {conta.VerSaldo()}");
+
+            conta.Sacar(3000);
+
+            Console.WriteLine($"O saldo em conta é: {conta.VerSaldo()}");
+
+
+        }
+    }
+}
---
+++ SistemaBancarioExemplo/SistemaBancario/Conta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancarioExemplo.SistemaBancario
+{
+    public class Conta
+    {
+        public int Agencia { get; set; }
+        public int NumeroConta { get; set; }
+        public string? NomeConta { get; set; }
+        public Titular? Titular { get; set; }
+
+        public decimal SaldoConta { get; set; }
+
+        public decimal VerSaldo()
+        {
+            return SaldoConta;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            SaldoConta += valor;
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor <= SaldoConta)
+                SaldoConta -= valor;
+            else
+                Console.WriteLine("Saldo Insuficiente");
+        }
+    }
+}
